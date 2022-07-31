7f389e4dac2f85bcfb8b69507b87ae04461b938e
ashikur37 <piash3700@gmail.com>
2022-07-31 10:45:28 +0600

    updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import "../assets/css/style.css";
 import "../assets/css/bootstrap.min.css";
-import "../assets/css/fontawsom-all.min.css";
-import "../assets/plugins/grid-gallery/css/grid-gallery.min.css";
+// import "../assets/css/fontawsom-all.min.css";
+// import "../assets/plugins/grid-gallery/css/grid-gallery.min.css";
 import Header from '../components/layout/Header';
 import Footer from '../components/layout/Footer';
 
